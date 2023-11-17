d8e19a2d17ad9201d8cf8ec7d7a778cb2da4907b
mvidalt <mvidalt@cifpfbmoll.eu>
2023-11-17 20:56:43 +0100

    Task 3 v2 Añadimos GridLayout y boton para añadir comentario

--- app/src/main/java/com/example/scrollingtext/MainActivity.java
+++ app/src/main/java/com/example/scrollingtext/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.scrollingtext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText editText;
+    private Button addButtonComment;
+    private TextView comentarios;
+    private int contadorMensajes = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.miEditText);
+        addButtonComment = findViewById(R.id.addButtonComment);
+        comentarios = findViewById(R.id.comentarios);
+
+        addButtonComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setvisible();
+                saveComment();
+            }
+        });
+    }
+
+    public void setvisible(){
+        editText.setVisibility(View.VISIBLE);
+        addButtonComment.setText("SEND COMMENT");
+    }
+
+    public void saveComment(){
+        if(addButtonComment.getText().toString().equals("SEND COMMENT")){
+            String nuevoMensaje = editText.getText().toString().trim(); // Eliminar espacios en blanco al principio y al final
+
+            if (!nuevoMensaje.isEmpty()) {
+                String mensajeConNumero = "Mensaje " + contadorMensajes + ": " + nuevoMensaje;
+                contadorMensajes++;
+
+                String mensajeExistente = comentarios.getText().toString();
+                String mensajeCompleto;
+
+                if (mensajeExistente.isEmpty()) {
+                    mensajeCompleto = mensajeConNumero;
+                } else {
+                    mensajeCompleto = mensajeExistente + "\n\n" + mensajeConNumero;
+                }
+
+                comentarios.setText(mensajeCompleto);
+                Log.i("EditText", editText.getText().toString());
+            }
+        }
     }
 }
